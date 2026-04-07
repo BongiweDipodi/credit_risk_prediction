a9a08bb79de12cb5ba32c96e1f9190dcd79a8dab
Bongiwe Dipodi <bodipjhb025@student.wethinkcode.co.za>
2026-04-07 13:37:06 +0200

    added data

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,1 +1,22 @@
+# # src/data_preprocessing.py
+# import pandas as pd
 
+# def load_data(file_path):
+#     """Load CSV file into a Pandas DataFrame"""
+#     df = pd.read_csv(file_path)
+#     return df
+
+# def clean_data(df):
+#     """Perform basic data cleaning"""
+
+#     df = df.drop('clientid', axis=1)
+    
+
+#     df = df.dropna()
+    
+#     return df
+
+# if __name__ == "__main__":
+#     df = load_data("../data/original.csv")
+#     df = clean_data(df)
+#     print(df.head())
